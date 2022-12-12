c45cb7f0bcc6b1a4884efbda0db1b9cc6dc6745f
Yugiobattu420 <nhatduy0409@gmail.com>
2022-12-12 20:48:13 +0700

    Add listService

--- BackEnd/src/controllers/listController.js
+++ BackEnd/src/controllers/listController.js
@@ -1,41 +1,9 @@
-import { where } from "sequelize";
-import db from "../models/index";
-import moment from "moment/moment";
+import listService from "../services/listService"
 
 let getListPage = async (req, res) => {
     try {
-        let items = await db.Ticket.findAll({
-            raw: true,
-        });
-        // console.log("All:", JSON.stringify(items, null, 2));
-        items.forEach(async (item) => {
-            let dateStart = moment(new Date(item.start));
-            let dateEnd = moment(new Date(item.end));
-
-            console.log(dateStart)
-            let carOwner = await db.CarOwner.findOne({
-                attributes: ["name"],
-                where: { id: item.idCarOwner },
-            });
-            let FromTo = await db.Trip.findOne({
-                where: { id: item.idTrip },
-            });
-            let seatBlank = await db.Seat.findAndCountAll({
-                where: {
-                    idTicket: item.id,
-                    idUser: null
-                }
-            })
-            item.carOwnerName = carOwner.name;
-            item.from = FromTo.from;
-            item.to = FromTo.to;
-            item.timeStart = dateStart.hour() + ":" + dateStart.minute();
-            item.timeEnd = dateEnd.hour() + ":" + dateEnd.minute();
-            item.price = item.price + " VND/";
-            item.black = seatBlank
-            item.hours = moment.duration(dateEnd.diff(dateStart)).asHours();
-        });
-        res.locals.list = items;
+        let list = await listService.getTicketInfo(req.query)
+        res.locals.list = list;
         res.render("list", {
             style: "css/list.css",
         });
---
+++ BackEnd/src/services/listService.js
@@ -0,0 +1,49 @@
+import db from "../models/index";
+import moment from "moment/moment";
+
+let getTicketInfo = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let items = await db.Ticket.findAll({
+                raw: true,
+            });
+            items.map(async (item) => {
+                let dateStart = moment(new Date(item.start));
+                let dateEnd = moment(new Date(item.end));
+
+                let carOwner = await db.CarOwner.findOne({
+                    attributes: ["name"],
+                    where: { id: item.idCarOwner },
+                });
+
+                let FromTo = await db.Trip.findOne({
+                    where: { id: item.idTrip },
+                });
+
+                let seatBlank = await db.Seat.findAndCountAll({
+                    where: {
+                        idTicket: item.id,
+                        idUser: null
+                    }
+                })
+
+                item.carOwnerName = carOwner.name;
+                item.from = FromTo.from;
+                item.to = FromTo.to;
+                item.timeStart = dateStart.hour() + ":" + dateStart.minute();
+                item.timeEnd = dateEnd.hour() + ":" + dateEnd.minute();
+                item.price = item.price + " VND/";
+                item.black = seatBlank
+                item.hours = moment.duration(dateEnd.diff(dateStart)).asHours();
+            });
+
+            resolve(items)
+        } catch (e) {
+            console.log(e)
+        }
+    })
+}
+
+module.exports = {
+    getTicketInfo
+}
