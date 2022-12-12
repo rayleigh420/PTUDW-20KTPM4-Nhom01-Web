b0a955614a4c9648a47329dd46fc920d227711db
Yugiobattu420 <nhatduy0409@gmail.com>
2022-12-12 20:25:33 +0700

    Caculate hours between two date and add moment'

--- BackEnd/src/controllers/listController.js
+++ BackEnd/src/controllers/listController.js
@@ -1,14 +1,18 @@
 import { where } from "sequelize";
 import db from "../models/index";
+import moment from "moment/moment";
 
 let getListPage = async (req, res) => {
     try {
-        let items = await db.Ticket.findAll();
-        console.log(items)
+        let items = await db.Ticket.findAll({
+            raw: true,
+        });
         // console.log("All:", JSON.stringify(items, null, 2));
         items.forEach(async (item) => {
-            let dateStart = new Date(item.start);
-            let dateEnd = new Date(item.end);
+            let dateStart = moment(new Date(item.start));
+            let dateEnd = moment(new Date(item.end));
+
+            console.log(dateStart)
             let carOwner = await db.CarOwner.findOne({
                 attributes: ["name"],
                 where: { id: item.idCarOwner },
@@ -25,10 +29,11 @@ let getListPage = async (req, res) => {
             item.carOwnerName = carOwner.name;
             item.from = FromTo.from;
             item.to = FromTo.to;
-            item.timeStart = dateStart.getHours() + ":" + dateStart.getMinutes();
-            item.timeEnd = dateEnd.getHours() + ":" + dateEnd.getMinutes();
+            item.timeStart = dateStart.hour() + ":" + dateStart.minute();
+            item.timeEnd = dateEnd.hour() + ":" + dateEnd.minute();
             item.price = item.price + " VND/";
             item.black = seatBlank
+            item.hours = moment.duration(dateEnd.diff(dateStart)).asHours();
         });
         res.locals.list = items;
         res.render("list", {
