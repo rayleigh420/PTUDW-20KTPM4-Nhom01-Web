61f1f005f548ab61729113247e7d152ef907260e
Baguette-Enjoyer <klong31122001@gmail.com>
2022-12-14 22:20:20 +0700

    fix something

--- BackEnd/src/controllers/detailController.js
+++ BackEnd/src/controllers/detailController.js
@@ -1,18 +1,16 @@
-import detailService from "../services/listService"
+import detailService from "../services/listService";
 
 let getDetailPage = async (req, res) => {
-    try {
-        res.render("detail", {
-            style: "css/detail.css",
-            js: ["detail.js"]
-        });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    res.render("detail", {
+      style: "../css/detail.css",
+      js: ["detail.js"],
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 module.exports = {
-    getDetailPage,
+  getDetailPage,
 };
-
-
--- BackEnd/src/resources/public/js/detail.js
+++ BackEnd/src/resources/public/js/detail.js
@@ -1,19 +1,19 @@
 hoverSeat = () => {
-    let seats = document.querySelectorAll(".seat");
-    for (let i = 0; i <= seats.length - 1; i++) {
-        seats[i].addEventListener(
-            "mouseover",
-            (hover = () => {
-                seats[i].src = "img/seat-hover.png";
-            })
-        );
-        seats[i].addEventListener(
-            "mouseout",
-            (handleHover = () => {
-                seats[i].src = "img/seat.png";
-            })
-        );
-    }
+  let seats = document.querySelectorAll(".seat");
+  for (let i = 0; i <= seats.length - 1; i++) {
+    seats[i].addEventListener(
+      "mouseover",
+      (hover = () => {
+        seats[i].src = "../img/seat-hover.png";
+      })
+    );
+    seats[i].addEventListener(
+      "mouseout",
+      (handleHover = () => {
+        seats[i].src = "../img/seat.png";
+      })
+    );
+  }
 };
 
 hoverSeat();
