f10d67bbb751f58f270a25164956fa00d45c588e
rayleigh420 <nhatduy0409@gmail.com>
2022-12-16 20:55:14 +0700

    Add localstorage when sign up

--- BackEnd/src/resources/public/js/signUp.js
+++ BackEnd/src/resources/public/js/signUp.js
@@ -13,26 +13,44 @@ eyePassword.forEach(item => {
     }
 })
 
+let email = document.querySelector('#email')
 let password = document.querySelector('#password')
-let rePassword = document.querySelector('#rePassword')
-
+let re_password = document.querySelector('#rePassword')
+let user = {
+    isLogin: false
+}
 
 let btnSubmit = document.querySelector('.btn-submit');
-// btnSubmit.onclick = async (e) => {
-//     e.preventDefault();
+btnSubmit.onmouseover = async () => {
+    if (password.value == re_password.value) {
+        if (email.value && password.value) {
 
-//     let res = await fetch("/signUp", {
-//         method: 'post',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             a: "1",
-//             b: "2"
-//         }),
-//     })
-//     console.log("res: ", res);
-//     if (password.value == rePassword.value) {
-//     }
+            let res = await fetch('user/signUp', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email.value,
+                    password: password.value
+                }),
+            })
 
-// }
+            let result = await res.json()
+            console.log(result)
+            if (result.errCode == 0) {
+                user = {
+                    isLogin: true,
+                    email: result.user.email,
+                    name: result.user.name
+                }
+            }
+        }
+    }
+}
+
+btnSubmit.onclick = () => {
+    if (user.isLogin) {
+        localStorage.setItem('Auth', JSON.stringify(user))
+    }
+}
