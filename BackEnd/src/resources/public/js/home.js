db7964746a96a29181f1cc49ff246edede0896bd
rayleigh420 <nhatduy0409@gmail.com>
2022-12-19 10:35:53 +0700

    Save infomation of find to local

--- BackEnd/src/resources/public/js/home.js
+++ BackEnd/src/resources/public/js/home.js
@@ -1,8 +1,56 @@
-let fromOption = document.querySelector('select.from')
-let toOption = document.querySelectorAll('select.to option')
+let find = JSON.parse(localStorage.getItem('Find'))
+if (find) {
+    let fromOption = document.querySelectorAll('select.from option')
+    let toOption = document.querySelectorAll('select.to option')
+    let date = document.querySelector('#date')
+
+    fromOption.forEach(item => {
+        if (item.value == find.from) {
+            item.setAttribute('selected', true)
+        }
+        else {
+            item.removeAttribute('selected')
+        }
+    });
+
+    toOption.forEach(item => {
+        if (item.value == find.to) {
+            item.setAttribute('selected', true)
+        }
+        else {
+            item.removeAttribute('selected')
+        }
+    });
+
+    find.date = new Date(find.date)
+
+    let yyyy = find.date.getFullYear();
+    let mm = find.date.getMonth() + 1; // Months start at 0!
+    let dd = find.date.getDate();
+
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    let date_format = yyyy + "-" + mm + "-" + dd;
+    date.value = date_format
+}
 
 
-console.log(123)
-localStorage.setItem("lastname", "Smith");
+let form = document.querySelector('#form_find')
+form.onsubmit = () => {
+    let fromOption = document.querySelector('select.from')
+    let toOption = document.querySelector('select.to')
+    let date = document.querySelector('#date')
+
+    localStorage.setItem('Find', JSON.stringify({
+        from: fromOption.value,
+        to: toOption.value,
+        date: date.value
+    }))
+}
 
 
