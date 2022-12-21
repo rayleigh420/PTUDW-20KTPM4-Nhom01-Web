8a7ff4b46214e8ca81d5f3e830eec5dfaf4268f3
rayleigh420 <nhatduy0409@gmail.com>
2022-12-21 10:48:28 +0700

    Filter carOwner in list ticket page version 1.0

--- BackEnd/src/resources/public/js/list.js
+++ BackEnd/src/resources/public/js/list.js
@@ -1,4 +1,5 @@
 let ticketList = document.querySelectorAll('.list_ticket .ticket')
+
 let ownerName = "";
 let typeName = "";
 let price = "";
@@ -18,26 +19,40 @@ let insertTicket = (ticketFilter) => {
     })
 }
 
+let resetOwnerName = () => {
+    let ticketListCopy = [...ticketList]
+    ticketListCopy = ticketListCopy.filter(item => {
+        let name = item.querySelector('.car_name span')
+        return name.textContent != ownerName
+
+    })
+    insertTicket(ticketListCopy)
+}
+
 let filterTicket = () => {
     let ticketListCopy = [...ticketList]
     ticketListCopy = ticketListCopy.filter(item => {
         let name = item.querySelector('.car_name span')
-        if (ownerName == '') {
-            return name.textContent != ownerName
-        }
-        else {
-            return name.textContent == ownerName
-        }
+        return name.textContent == ownerName
     })
     insertTicket(ticketListCopy)
 }
 
 let carOwner = document.querySelector(".carOwner_option")
 carOwner.onchange = () => {
-    ownerName = carOwner.value
-    filterTicket()
+    if (carOwner.value == "") {
+        resetOwnerName();
+    }
+    else {
+        ownerName = carOwner.value
+        filterTicket()
+    }
 }
 
 let type = document.querySelector(".type_option")
 type.onchange = () => {
+    // if (type.value == ''){
+    //     reset
+    // }
+    // filterTicket()
 }
