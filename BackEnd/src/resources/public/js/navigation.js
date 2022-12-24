e8f136c8d1412a4fc1b91295a5690de5d1dfb498
rayleigh420 <nhatduy0409@gmail.com>
2022-12-24 19:56:27 +0700

    Fix navigation signIn signUP

--- BackEnd/src/resources/public/js/navigation.js
+++ BackEnd/src/resources/public/js/navigation.js
@@ -1,5 +1,5 @@
 let auth = JSON.parse(localStorage.getItem('Auth'))
-let checkUser = false
+// let checkUser;
 
 const check = async () => {
     let res = await fetch('user/checkLocal', {
@@ -13,53 +13,51 @@ const check = async () => {
         }),
     })
     let result = await res.json()
-    checkUser = result
-    if (!result) {
+
+    let userNav = document.querySelector('.btn_auth')
+    if (result) {
+        userNav.innerHTML = `
+        <button type="button" class="btn btn_user">
+        <ion-icon class="user_icon" name="person-circle"></ion-icon>
+        ${auth.name}
+        </button>
+        <div class="list">
+        <ul class="list-group">
+        <li class="list-group-item list_title">
+        Tài khoản
+        </li>
+        <li class="list-group-item">
+        <a href="/history">
+        <ion-icon class="user_option_icon" name="book" size="small"></ion-icon>Lịch sử đặt vé
+        </a>
+        </li>
+        <li class="list-group-item log_out">
+        <a href="/">
+        <ion-icon class="user_option_icon" name="log-out" size="small"></ion-icon>Đăng xuất
+        </a>
+                </li>
+            </ul>
+        </div> `
+    }
+    else {
         localStorage.removeItem('Auth')
+
+        userNav.innerHTML = `
+        <button type="button" class="btn btn_signIn">
+        <a href="/signIn">
+        <ion-icon class="signIn_icon" name="people-circle"></ion-icon>
+        Đăng nhập
+        </a>
+        </button>
+        <button type="button" class="btn btn_signUp">
+            <a href="/signUp">
+                Đăng ký
+            </a>
+        </button> `
     }
 }
 
 check();
-
-let userNav = document.querySelector('.btn_auth')
-if (checkUser) {
-    userNav.innerHTML = `
-    <button type="button" class="btn btn_user">
-        <ion-icon class="user_icon" name="person-circle"></ion-icon>
-        ${auth.name}
-    </button>
-    <div class="list">
-        <ul class="list-group">
-            <li class="list-group-item list_title">
-                Tài khoản
-            </li>
-            <li class="list-group-item">
-                <a href="/history">
-                    <ion-icon class="user_option_icon" name="book" size="small"></ion-icon>Lịch sử đặt vé
-                </a>
-            </li>
-            <li class="list-group-item log_out">
-                <a href="/">
-                    <ion-icon class="user_option_icon" name="log-out" size="small"></ion-icon>Đăng xuất
-                </a>
-            </li>
-        </ul>
-    </div> `
-}
-else {
-    userNav.innerHTML = `
-    <button type="button" class="btn btn_signIn">
-        <a href="/signIn">
-            <ion-icon class="signIn_icon" name="people-circle"></ion-icon>
-            Đăng nhập
-        </a>
-    </button>
-    <button type="button" class="btn btn_signUp">
-        <a href="/signUp">
-            Đăng ký
-        </a>
-    </button> `
-}
 
 let logOut = document.querySelector('.log_out')
 if (logOut) {
