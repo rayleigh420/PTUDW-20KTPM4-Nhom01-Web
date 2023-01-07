32e8269b0519734d9512f745fb6569c3e63f48a6
rayleigh420 <nhatduy0409@gmail.com>
2023-01-07 19:33:54 +0700

    Fix name of info check page

--- BackEnd/src/resources/public/js/info_check.js
+++ BackEnd/src/resources/public/js/info_check.js
@@ -1,7 +1,7 @@
 let edit = document.querySelector('.edit_span')
 edit.onclick = () => {
     localStorage.setItem('Info', JSON.stringify({
-        name: document.querySelector('.name_span').textContent,
+        name: document.querySelector('.name_span span').textContent,
         phone: document.querySelector('.form_phone').value,
         email: document.querySelector('.form_email').value,
         cmnd: document.querySelector('.form_cmnd').value,
