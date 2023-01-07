8d7987f5374126f947df3d7df009d393ed0fcdfa
rayleigh420 <nhatduy0409@gmail.com>
2023-01-07 19:18:51 +0700

    ...

--- BackEnd/src/resources/public/js/history.js
+++ BackEnd/src/resources/public/js/history.js
@@ -1,13 +1,13 @@
 if (!JSON.parse(localStorage.getItem("Auth"))) {
-  location.assign("/");
+    location.assign("/");
 }
 let user = JSON.parse(localStorage.getItem("Auth"));
 console.log("Auth", user);
 if (user) {
-  let email = document.querySelector("#parseUser");
-  email.value = user.email;
+    let email = document.querySelector("#parseUser");
+    email.value = user.email;
 }
 let logOutELement = document.querySelector(".logout");
 logOutELement.onclick = () => {
-  localStorage.removeItem("Auth");
+    localStorage.removeItem("Auth");
 };
