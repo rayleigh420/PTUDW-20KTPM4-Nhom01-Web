47d0b8b1a12afb288d48f62ad31ddf23e099e223
rayleigh420 <nhatduy0409@gmail.com>
2023-01-09 19:43:06 +0700

    Complete UI form update trip

--- src/resources/public/js/tripAdmin.js
+++ src/resources/public/js/tripAdmin.js
@@ -1,3 +1,19 @@
+const detailList = document.querySelectorAll('.detail')
+detailList.forEach(item => {
+    const fromSelected = document.querySelector('input[name="fromSelected"]').value
+    const toSelected = document.querySelector('input[name="toSelected"]').value
+
+    console.log(fromSelected, toSelected)
+
+    const optionFrom = item.querySelector(`select[name="from"] option[value=${fromSelected}]`)
+    optionFrom.setAttribute("selected", "true")
+
+    const optionTo = item.querySelector(`select[name="to"] option[value=${toSelected}]`)
+    optionTo.setAttribute("selected", "true")
+})
+
+
+
 const show = (id) => {
     const detailList = document.querySelectorAll('.detail')
     const detail = document.querySelector(`.detail_${id}`)
