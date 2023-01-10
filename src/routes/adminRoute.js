eea0f832d13ddcd9b9d1c3146ae12474e58b0f61
Baguette-Enjoyer <klong31122001@gmail.com>
2023-01-10 20:16:52 +0700

    add admin update ticket

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -4,28 +4,29 @@ import adminController from "../controllers/adminController";
 let router = express.Router();
 
 const initAdminRoute = (app) => {
-    router.get("/", adminController.getAdminPage);
+  router.get("/", adminController.getAdminPage);
 
-    router.get("/adminCarOwner", adminController.getCarOwnerAdmin);
-    router.get("/adminTrip", adminController.getTripAdmin);
-    router.get("/adminSeat", adminController.getSeatAdmin);
-    router.get("/adminTicket", adminController.getTicketAdmin);
+  router.get("/adminCarOwner", adminController.getCarOwnerAdmin);
+  router.get("/adminTrip", adminController.getTripAdmin);
+  router.get("/adminSeat", adminController.getSeatAdmin);
+  router.get("/adminTicket", adminController.getTicketAdmin);
 
-    router.post("/addCarOwner", adminController.addCarOwner);
-    router.post("/updateCarOwner/:id", adminController.updateCarOwner);
-    router.get("/deleteCarOwner/:id", adminController.deleteCarOwner);
+  router.post("/addCarOwner", adminController.addCarOwner);
+  router.post("/updateCarOwner/:id", adminController.updateCarOwner);
+  router.get("/deleteCarOwner/:id", adminController.deleteCarOwner);
 
-    router.post("/addTrip", adminController.addTrip);
-    router.post("/updateTrip/:id", adminController.updateTrip);
-    router.get("/deleteTrip/:id", adminController.deleteTrip);
+  router.post("/addTrip", adminController.addTrip);
+  router.post("/updateTrip/:id", adminController.updateTrip);
+  router.get("/deleteTrip/:id", adminController.deleteTrip);
 
-    router.post("/updateSeat/:id", adminController.updateSeat);
+  router.post("/updateSeat/:id", adminController.updateSeat);
 
-    router.get("/adminTicketTest", adminController.testInsert);
-    router.post("/addTicket", adminController.addTicket);
-    router.get("/deleteTicket/:id", adminController.deleteTicket);
+  router.get("/adminTicketTest", adminController.testInsert);
+  router.post("/addTicket", adminController.addTicket);
+  router.get("/deleteTicket/:id", adminController.deleteTicket);
+  router.post("/updateTicket/:id", adminController.updateTicket);
 
-    return app.use("/admin", router);
+  return app.use("/admin", router);
 };
 
 export default initAdminRoute;
