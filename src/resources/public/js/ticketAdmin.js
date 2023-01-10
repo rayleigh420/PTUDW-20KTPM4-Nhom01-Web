7c90b5e2bfc61c9d6a421bc75e5a734a9c1fc4c9
rayleigh420 <nhatduy0409@gmail.com>
2023-01-10 19:39:03 +0700

    Add something dom event ticket admin page

--- src/resources/public/js/ticketAdmin.js
+++ src/resources/public/js/ticketAdmin.js
@@ -1,5 +1,26 @@
 const detailList = document.querySelectorAll(".detail");
 
+detailList.forEach(item => {
+    const carOwnerSelected = item.querySelector('input[name="carOwnerSelected"]').value
+    // const fromSelected = item.querySelector('input[name="fromSelected"]').value
+    // const toSelected = item.querySelector('input[name="toSelected"]').value
+
+    // console.log(fromSelected, toSelected)
+
+    // const optionFrom = item.querySelector(`select[name="fromPlace"] option[value="${fromSelected}"]`)
+    // optionFrom.setAttribute("selected", "true")
+
+    // const optionTo = item.querySelector(`select[name="toPlace"] option[value="${toSelected}"]`)
+    // optionTo.setAttribute("selected", "true")
+
+    const optionCarOwner = item.querySelectorAll(`select[name="idCarOwner"] option`)
+    optionCarOwner.forEach(item => {
+        if (item.innerText == carOwnerSelected) {
+            item.setAttribute("selected", "true")
+        }
+    })
+})
+
 const show = (id) => {
     const detailList = document.querySelectorAll(".detail");
     const detail = document.querySelector(`.detail_${id}`);
