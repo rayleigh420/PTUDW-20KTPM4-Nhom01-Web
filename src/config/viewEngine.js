24e71f9d34dc4677d3d521b2f351db6c0eece569
Baguette-Enjoyer <klong31122001@gmail.com>
2023-01-10 21:23:01 +0700

    ...

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -4,24 +4,27 @@ import { engine } from "express-handlebars";
 const helper = require("../controllers/helper");
 
 const configViewEngine = (app, pathName) => {
-    app.engine("hbs", engine({
-        layoutsDir: path.join(pathName, "resources", "views", "layouts"),
-        partialsDir: path.join(pathName, "resources", "views", "partials"),
-        defaultLayout: "layout",
-        helpers: {
-            generateTypeOfCar: helper.generateTypeOfCar,
-            generateStarList: helper.generateStarList,
-        },
-        extname: "hbs",
-        runtimeOptions: {
-            allowProtoPropertiesByDefault: true,
-        },
+  app.engine(
+    "hbs",
+    engine({
+      layoutsDir: path.join(pathName, "resources", "views", "layouts"),
+      partialsDir: path.join(pathName, "resources", "views", "partials"),
+      defaultLayout: "layout",
+      helpers: {
+        generateTypeOfCar: helper.generateTypeOfCar,
+        generateStarList: helper.generateStarList,
+        generateText: helper.generateText,
+      },
+      extname: "hbs",
+      runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+      },
     })
-    );
+  );
 
-    app.set("view engine", "hbs");
-    app.set("views", path.join(pathName, "resources", "views"));
-    app.use(express.static(path.join(pathName, "resources", "public")));
+  app.set("view engine", "hbs");
+  app.set("views", path.join(pathName, "resources", "views"));
+  app.use(express.static(path.join(pathName, "resources", "public")));
 };
 
 export default configViewEngine;
--- src/controllers/helper.js
+++ src/controllers/helper.js
@@ -1,9 +1,21 @@
 let helper = {};
-
+helper.generateText = (type) => {
+  let str = ``;
+  if (type == 9) {
+    str = `<div class="type">Loại xe: xe Limousine 9 chỗ</div>`;
+  } else if (type == 22) {
+    str = `<div class="type">Loại xe: xe giường nằm 22 chỗ</div>`;
+  } else if (type == 32) {
+    str = `<div class="type">Loại xe: xe giường nằm 32 chỗ</div>`;
+  } else {
+    str = `<div class="type">Loại xe: xe Limousine 9 chỗ</div>`;
+  }
+  return str;
+};
 helper.generateTypeOfCar = (type) => {
-    let str = "";
-    if (type == 9) {
-        str = `<div class="car-9">
+  let str = "";
+  if (type == 9) {
+    str = `<div class="car-9">
               <img class="seat" src="/img/seat.png" alt="">
               <img class="seat" src="/img/seat.png" alt="">
               <img class="seat" src="/img/seat.png" alt="">
@@ -18,8 +30,8 @@ helper.generateTypeOfCar = (type) => {
               <img class="seat" src="/img/seat.png" alt="">
 
           </div>`;
-    } else if (type == 32) {
-        str = `<div class="floor">Tầng dưới</div>
+  } else if (type == 32) {
+    str = `<div class="floor">Tầng dưới</div>
         <div class="car-32">
             <div class="blank"></div>
             <img class="seat" src="/img/seat.png" alt="">
@@ -67,8 +79,8 @@ helper.generateTypeOfCar = (type) => {
             <img class="seat" src="/img/seat.png" alt="">
             <img class="seat" src="/img/seat.png" alt="">
         </div> `;
-    } else if (type == 22) {
-        str = `<div class ="floor">Tầng dưới</div>
+  } else if (type == 22) {
+    str = `<div class ="floor">Tầng dưới</div>
         <div class="car-22">
             <div class="blank"></div>
             <img class="seat" src="/img/seat.png" alt="">
@@ -100,31 +112,47 @@ helper.generateTypeOfCar = (type) => {
             <img class="seat" src="/img/seat.png" alt="">
             
         </div> `;
-    }
-    return str;
+  } else {
+    str = `<div class="car-9">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <div class="blank"></div>
+              <div class="blank"></div>
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="wheel" src="/img/wheel.svg" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+              <img class="seat" src="/img/seat.png" alt="">
+
+          </div>`;
+  }
+  return str;
 };
 
 helper.generateStarList = (stars) => {
-    let str = ``;
-    let starCanDraw = stars;
-    let i = 0;
-    for (i; i < stars; i++) {
-        str += `<i class="fa fa-star"></i>`;
-        starCanDraw -= 1;
-        if (starCanDraw > 0 && starCanDraw < 1) {
-            if (starCanDraw >= 0.5) {
-                str += `<i class="fa fa-star-half-o"></i>`;
-                i++;
-                i++; //lag
-                break;
-            }
-        }
+  let str = ``;
+  let starCanDraw = stars;
+  let i = 0;
+  for (i; i < stars; i++) {
+    str += `<i class="fa fa-star"></i>`;
+    starCanDraw -= 1;
+    if (starCanDraw > 0 && starCanDraw < 1) {
+      if (starCanDraw >= 0.5) {
+        str += `<i class="fa fa-star-half-o"></i>`;
+        i++;
+        i++; //lag
+        break;
+      }
     }
-    for (i; i < 5; i++) {
-        str += `<i class="fa fa-star-o"></i>`;
-    }
+  }
+  for (i; i < 5; i++) {
+    str += `<i class="fa fa-star-o"></i>`;
+  }
 
-    return str;
+  return str;
 };
 
 module.exports = helper;
